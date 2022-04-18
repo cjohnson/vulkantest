8808a4f726932d037327102ac56e09801664890f
cjohnson <collin.g.johnson@gmail.com>
2022-04-18 17:40:39 -0400

    Add shader pipeline

--- Makefile
+++ Makefile
@@ -4,9 +4,12 @@ LDFLAGS = -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
 VulkanTest: *.cpp *.hpp
 	g++ $(CFLAGS) -o VulkanTest *.cpp $(LDFLAGS)
 
-.PHONY: test clean
+.PHONY: exec clean shaders
 
-test: VulkanTest
+shaders: VulkanTest
+	./compile.sh
+
+exec: shaders
 	./VulkanTest
 
 clean:
--- first_app.hpp
+++ first_app.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "lve_window.hpp"
+#include "lve_pipeline.hpp"
 
 namespace lve {
 
@@ -13,6 +14,7 @@ class FirstApp {
 
     private:
         LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+        LvePipeline lvePipeline{"shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv"};
 };
 
 }
