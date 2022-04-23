bd07eba174aed17d7c4a1d81be89592734651411
cjohnson <collin.g.johnson@gmail.com>
2022-04-22 23:34:49 -0400

    Add pipeline layout construction to main class functions

--- first_app.hpp
+++ first_app.hpp
@@ -3,6 +3,10 @@
 #include "lve_window.hpp"
 #include "lve_pipeline.hpp"
 #include "lve_device.hpp"
+#include "lve_swap_chain.hpp"
+
+#include <memory>
+#include <vector>
 
 namespace lve {
 
@@ -11,17 +15,26 @@ class FirstApp {
         static constexpr int WIDTH = 800;
         static constexpr int HEIGHT = 600;
 
+        FirstApp();
+        ~FirstApp();
+
+        FirstApp(const FirstApp &) = delete;
+        FirstApp &operator=(const FirstApp &) = delete;
+
         void run();
 
     private:
+        void createPipelineLayout();
+        void createPipeline();
+        void createCommandBuffers();
+        void drawFrame();
+
         LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
         LveDevice lveDevice{lveWindow};
-        LvePipeline lvePipeline{
-            lveDevice,
-            "build/shaders/simple_shader.vert.spv", 
-            "build/shaders/simple_shader.frag.spv",
-            LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)
-        };
+        LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+        std::unique_ptr<LvePipeline> lvePipeline;
+        VkPipelineLayout pipelineLayout;
+        std::vector<VkCommandBuffer> commandBuffers;
 };
 
 }
